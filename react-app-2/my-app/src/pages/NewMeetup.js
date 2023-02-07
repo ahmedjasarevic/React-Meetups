920aaf322b377a2b1cefc404e3454203cee7d5e7
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2023-02-07 12:37:33 +0100

    Add a post request that save it to FireBase

--- react-app-2/my-app/src/pages/NewMeetup.js
+++ react-app-2/my-app/src/pages/NewMeetup.js
@@ -1,10 +1,27 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useNavigate } from 'react-router-dom';
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-15304-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: 'POST', 
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type' : 'application/json'
+        }
+      }
+    ).then(() => {
+      navigate("/", { replace: true });
+    });
+  }
   return (
     <section>
       <h1>Add a new Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
